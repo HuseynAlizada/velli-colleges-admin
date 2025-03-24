28c9f3904dc65921f6698237a8beb490e51aa5ef
HuseynDev <huseyn.alizada.23@gmail.com>
2025-03-24 11:48:44 +0400

    config changes

--- src/utils/supabase-client.ts
+++ src/utils/supabase-client.ts
@@ -1,10 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+import { createClient } from '@supabase/supabase-js'
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Missing Supabase environment variables");
-}
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
