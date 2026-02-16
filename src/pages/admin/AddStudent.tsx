b64b650bff199481d47eafc6a1752bb0e46f567f
HuseynDev <huseyn.alizada.23@gmail.com>
2026-02-16 13:07:45 +0400

    changess

--- src/pages/admin/AddStudent.tsx
+++ src/pages/admin/AddStudent.tsx
@@ -14,6 +14,8 @@ const levels = [
   { id: "b1+", name: "B1+" },
   { id: "b2", name: "B2" },
   { id: "c1", name: "C1" },
+
+
   // { id: "placement-test", name: "Placement Test" },
   // {id:"kids", name:"Kids" },
 ];
@@ -22,6 +24,7 @@ const placementTest = [{ id: true, name: "Placement Test" }];
 const kidsTest = [{ id: "kids-test", name: "Kids" }];
 const ieltsTofel = [
   { id: "ielts", name: "IELTS" },
+  
   { id: "tofel", name: "TOFEL" },
 ];
 
